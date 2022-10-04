b9c398575aff522625d132438e6ed07001f6faf4
Sajan Gurung <sggrggrg@gmail.com>
2022-10-04 18:41:21 +0545

    Updates in stories

--- wp-content/themes/twentytwentytwo/templates/template-stories.php
+++ wp-content/themes/twentytwentytwo/templates/template-stories.php
@@ -24,8 +24,8 @@ get_header(); ?>
     <div class="row">
       <div class="col-lg-12 py-5">
         <div class="service-text-content">
-          <h1>Stories</h1>
-          <strong class="d-block mb-3">Latest Story: Andrina and Donna</strong>
+          <strong class="d-block mb-1">Latest Story</strong>
+          <h1>Andrina and Donna</h1>
           <p>Do you enjoy sharing in moments of human experience? Are you empathetic, caring and good at building relationships? Would you love a job where you can choose your own hours with competitive pay and consistent shifts?</p>
           <p>If so, we think you might make the perfect Support Star, and we’re hiring now!</p>
           <p>We’re looking for stars with great people skills, who are patient, compassionate, and have a smile on their face. People who want to make a difference in the life of others and are part of their local community. We are an organisation that embraces diversity and individuality and likes to have fun!</p>
@@ -36,53 +36,49 @@ get_header(); ?>
     </div>
   </div>
 </section>
-<section class="section home-stories py-5">
+<section class="section stories-list py-5">
   <div class="container">
     <div class="row align-items-center">
       <div class="col-lg-12">
-        <div class="owl-carousel stories-carousel">
-          <div class="item">
-            <div class="row align-items-center story-block d-flex w-100">
-              <div class="col-lg-6">
-                <div class="stories-content-wrapper">
-                  <span>Stores #1</span>
-                  <a href="#" class="story-heading">
-                    <h1>Chloe &amp; Catherine</h1>
-                  </a>
-                  <p>Chloe and Catherine are the perfect match! After a skiing accident in 2017, Chloe was left paralysed. Catherine has been there through the ups and the downs of Chloe’s journey, supporting her with her daily routine, household cleaning and as a new found friend.</p>
-                  <a href="#" class="btn btn-primary text-white">Watch</a>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="video-thumbnail-stories-wrapper">
-                  <img src="<?php bloginfo('template_directory');?>/assets/images/Nurse_and_client_smiling_together_banner_large.jpg" alt="High Level Support" class="img-fluid">
-                  <a href="javascript:void(0)" data-href="https://vimeo.com/539344382" class="venobox video-icon" data-autoplay="true" data-vbtype="video">
-                    <img src="<?php bloginfo('template_directory');?>/assets/images/icons/play-icon.svg" alt="">
-                  </a>
-                </div>
-              </div>
+        <div class="story-inline-wrapper mb-5">
+          <div class="story-inline-img">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/Nurse_and_client_smiling_together_banner_large.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="story-inline-content">
+            <div class="stories-content-wrapper">
+              <a href="#" class="story-heading">
+                <h1>Chloe &amp; Catherine</h1>
+              </a>
+              <p>Chloe and Catherine are the perfect match! After a skiing accident in 2017, Chloe was left paralysed. Catherine has been there through the ups and the downs of Chloe’s journey, supporting her with her daily routine, household cleaning and as a new found friend.</p>
+              <a href="#" class="btn btn-secondary px-4">See The Watch</a>
             </div>
           </div>
-          <div class="item">
-            <div class="row align-items-center story-block d-flex w-100">
-              <div class="col-lg-6">
-                <div class="stories-content-wrapper">
-                  <span>Stores #2</span>
-                  <a href="#" class="story-heading">
-                    <h1>Chloe &amp; Catherine</h1>
-                  </a>
-                  <p>Chloe and Catherine are the perfect match! After a skiing accident in 2017, Chloe was left paralysed. Catherine has been there through the ups and the downs of Chloe’s journey, supporting her with her daily routine, household cleaning and as a new found friend.</p>
-                  <a href="#" class="btn btn-primary text-white">Watch</a>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="video-thumbnail-stories-wrapper">
-                  <img src="<?php bloginfo('template_directory');?>/assets/images/Nurse_and_client_smiling_together_banner_large.jpg" alt="High Level Support" class="img-fluid">
-                  <a href="javascript:void(0)" data-href="https://vimeo.com/539344382" class="venobox video-icon" data-autoplay="true" data-vbtype="video">
-                    <img src="<?php bloginfo('template_directory');?>/assets/images/icons/play-icon.svg" alt="">
-                  </a>
-                </div>
-              </div>
+        </div>
+        <div class="story-inline-wrapper mb-5">
+          <div class="story-inline-img">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/Nurse_and_client_smiling_together_banner_large.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="story-inline-content">
+            <div class="stories-content-wrapper">
+              <a href="#" class="story-heading">
+                <h1>Daniel, Chloe & Haley</h1>
+              </a>
+              <p>Chloe and Catherine are the perfect match! After a skiing accident in 2017, Chloe was left paralysed. Catherine has been there through the ups and the downs of Chloe’s journey, supporting her with her daily routine, household cleaning and as a new found friend.</p>
+              <a href="#" class="btn btn-secondary px-4">See The Watch</a>
+            </div>
+          </div>
+        </div>
+        <div class="story-inline-wrapper mb-5">
+          <div class="story-inline-img">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/Nurse_and_client_smiling_together_banner_large.jpg" alt="" class="img-fluid">
+          </div>
+          <div class="story-inline-content">
+            <div class="stories-content-wrapper">
+              <a href="#" class="story-heading">
+                <h1>Johnno & Robyn</h1>
+              </a>
+              <p>Chloe and Catherine are the perfect match! After a skiing accident in 2017, Chloe was left paralysed. Catherine has been there through the ups and the downs of Chloe’s journey, supporting her with her daily routine, household cleaning and as a new found friend.</p>
+              <a href="#" class="btn btn-secondary px-4">See The Watch</a>
             </div>
           </div>
         </div>
