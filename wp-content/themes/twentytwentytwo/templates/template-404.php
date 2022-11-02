8779a7d9da423286e9b22ae2e08943273a5b9d3d
Sajan Gurung <sggrggrg@gmail.com>
2022-11-02 21:29:59 +0545

    Added 404error and dropdown icon

--- wp-content/themes/twentytwentytwo/templates/template-404.php
+++ wp-content/themes/twentytwentytwo/templates/template-404.php
@@ -1,37 +1,22 @@
 <?php 
-/* Template Name: Team Template */ 
+/* Template Name: 404 Error Template */ 
 
 get_header(); ?>
-<section class="common-inner-heading">
-  <img src="<?php bloginfo('template_directory');?>/assets/images/lady-making-bed.png" class="img-fluid" alt="Inner Banner Image"/>
-  <div class="container">
-    <div class="row align-items-center justify-content-center">
-      <div class="col-md-12">
-        <div class="header-content">
-          <h1>Our People</h1>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="<?php echo home_url('/'); ?>">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Our people</li>
-            </ol>
-          </nav>
-        </div>
+<div class="error-wrapper px-3">
+   <div class="row justify-content-between text-center min-vh-100 row">
+      <div class="mb-0 col-lg-12">
       </div>
-    </div>
-  </div>
-</section>
-<section class="service-intro">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 py-5">
-        <div class="service-text-content">
-          <h1>Hearts of Alliance Community</h1>
-          <p>At Alliance Community we wanted to shine a light on our amazing Support Stars and internal team members. Our Support Stars and team across our branches make Alliance Community what it is and are the ones working with each other and our wonderful clients every day.</p>
-          <p>Our Support Stars and internal team love what they do and each have their own story to tell, that’s why we’re sharing the real profiles and experiences of the Hearts of Alliance Community!</p>
-        </div>
+      <div class="mb-4 col-lg-12">
+         <div class="error-info ">
+            <h1 class="highlight">404.</h1>
+            <h3 class="fw-bold">We couldn't find that page.</h3>
+         </div>
       </div>
-    </div>
-  </div>
-</section>
+      <div class="mb-4 col-lg-12">
+         <p class="mb-0">The page you are looking for does not exist or other error occured.</p>
+         <p class="mb-0"><a href="<?php echo get_home_url('/') ?>">Go back</a>, or head over to <a href="<?php echo get_home_url('/') ?>">gracefully-care.au</a> to choose a new direction</p>
+      </div>
+   </div>
+</div>
 <?php get_footer();?>
  
